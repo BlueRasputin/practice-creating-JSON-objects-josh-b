bb3ac794262c0ce356a0ad0b9f464da0c4dfe1a4
BlueRasputin <barrettjoshrev10@gmail.com>
2025-05-07 09:55:04 -0500

    That rug really tied the room together, man

---
+++ movie.js
@@ -0,0 +1,23 @@
+{
+    "title": "The Big Lebowski",
+    "year": 1998,
+    "isAvailable": true,
+    "genres": ["comedy", "slice-of-life", "adventure"],
+    "cast" : 
+        [
+            {   name: Jeff Bridges,
+                character: Jeff Lebowski,
+                role: lead
+            },
+
+            {   name:John Goodman,
+                character: Walter Sobchak,
+                role: supporting
+            },
+                
+            {   name: Steve Buscemi,
+                character: Donny Kerabatsos,
+                role: supporting
+            }
+        ],
+};
